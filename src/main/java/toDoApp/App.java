be30af6d06fa7d5a639230a9443cfa2bdf83085c
Patrycja <patrycja@localhost.localdomain>
2020-01-28 23:54:12 +0100

    close DB connection

--- src/main/java/toDoApp/App.java
+++ src/main/java/toDoApp/App.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import toDoApp.Utils.HibernateUtil;
 import toDoApp.notifier.Notifier;
 import toDoApp.notifier.NotifierOverdue;
 
@@ -22,10 +26,15 @@ public class App extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
         stage.setScene(new Scene(root));
         stage.setResizable(false);
+        stage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, windowEvent -> {
+            HibernateUtil.closeEntityManagerFactory();
+        });
         stage.show();
+
         Thread notifierThread = new Thread(new Notifier());
         notifierThread.setDaemon(true);
         notifierThread.start();
+
         Thread overdueThread = new Thread(new NotifierOverdue());
         overdueThread.setDaemon(true);
         overdueThread.start();
